aae0b3eb7fc52fa880cd0c69a2e6c784c176a4df
aaa1948 <101234734+aaa1948@users.noreply.github.com>
2026-04-07 16:42:21 +0900

    4월 7일
    2026-4-7일 웹페이지 작성

--- 0_var_dump.php
+++ 0_var_dump.php
@@ -1,10 +1,28 @@
 <?php
-// @TODO
+$username = 'Ivy'; //사칼라변수
+
+$user_array1 = ['Ivy', 34, 'Web programming']; //배열
+
+$user_array2 = [ //키/값 배열
+  'name'  => 'Ivy',
+  'age'   => 34,
+  'major' => 'Web programming',
+];
+
+class User {
+  public $name;
+  public $age;
+  public $major;
+  public function __construct($name, $age, $major) {
+    //정의할 때 매개변수
+    $this->name  = $name;
+    $this->age   = $age;
+    $this->major = $major;
+  }
+}
+// => 호출 할 때 인수
+$user_object = new User('Ivy', 34, 'Web programming');
 ?>
-<?php include 'includes/header.php' ?>
-
-  <p>Scalar: <?php var_dump($username); ?></p>
-  <p>Array:  <?php var_dump($user_array); ?></p>
-  <p>Object: <?php var_dump($user_object); ?></p>
-
-<?php include 'includes/footer.php' ?>
+<p>Scalar: <?php var_dump($username); ?></p>
+<p>Array:  <?php var_dump($user_array1); ?></p>
+<p>Object: <?php var_dump($user_array2); ?></p>
--- 4_multibyte-string-functions.php
+++ 4_multibyte-string-functions.php
@@ -1,7 +1,6 @@
 <?php
 $text = 'Total: £444';
 ?>
-<?php include 'includes/header.php'; ?>
 
 <p>
   <b>Character count using <code>strlen()</code>:</b>
@@ -14,4 +13,3 @@ $text = 'Total: £444';
   <br>
 </p>
 
-<?php include 'includes/footer.php'; ?>
